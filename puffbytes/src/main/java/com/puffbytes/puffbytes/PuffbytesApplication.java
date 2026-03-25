36495501e95b436f5908466021229e9928f160fd
shivam-verse <shivam@sentisum.com>
2026-03-25 12:08:22 +0530

    Initial commit

---
+++ puffbytes/src/main/java/com/puffbytes/puffbytes/PuffbytesApplication.java
@@ -0,0 +1,13 @@
+package com.puffbytes.puffbytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuffbytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuffbytesApplication.class, args);
+	}
+
+}
---
+++ puffbytes/src/test/java/com/puffbytes/puffbytes/PuffbytesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puffbytes.puffbytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuffbytesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
